c4c15aa1d35d0c66101398fa1d4111e616a797af
conor2779 <megax8348@gmail.com>
2025-10-17 09:07:09 +0300

    зробив ще завдання

---
+++ laba 3/ip.js
@@ -0,0 +1,8 @@
+const ip="10.0.0.1"
+const parts=ip.split('.')
+const number=parts.map(part=> Number(part))
+const total=(10<<24)+(0<<6)+(0<<8)+1
+const result =number.reduce((acc,num) => 
+    (acc<<8)+ num
+)
+console.log(result)
